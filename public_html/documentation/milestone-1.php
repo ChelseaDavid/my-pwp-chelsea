bc1a06963b67624de3a042d9482dc538e86f85a9
Chelsea David <cryan17@cnm.edu>
2017-07-16 16:39:40 -0600

    completed my pwp milestone one

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>my-pwp-chelsea</title>
+	</head>
+	<body>
+		<h1>Persona:</h1>
+		<img src="images/frame.jpg" alt="foundation">
+		<p><strong>Name:</strong> Juan Mendez</p>
+		<!-- User info / Persona -->
+		<p><strong>Age:</strong>34</p>
+		<p><strong>Operating Platforms:</strong> Spends most of his time out in the field so prefers to operate on his phone, a
+			Samsung Galaxy Edge 7, but occasionally works on his Windows Core i5 at home </p>
+		<p><strong>User:</strong>Juan is a simple man. He gets up early and begins his day by making breakfast for everyone
+			in his house and on his crew. He is in for load out by 7:00AM and in the field working by no later than 8AM. A
+			typical day for Juan is usually around 12-14 hours. He is constantly communicating with his crew and shop so
+			having a fast connection and thorough signal are key. </p>
+		<!-- User Story -->
+		<h2>User Story:</h2>
+
+		<p>As a user I want to allow other users to browse the work I have available, while simultaneously
+			establishing a customer base and strong resume/profile.</p>
+		<!-- Use Case -->
+		<h2>Use Case</h2>
+		<p>A user posts a job/resume by accessing the site, creating an account, posting a picture of previous installs with
+			detailed description of site and client. Further details may include pricing bids, and job site details.if user
+		has chosen to upload a resume for profile, ratings and reviews available from both an employers stand point as well
+		as the user is able to rate any person worked under him during any period of time.</p>
+		<!-- User Needs -->
+		<h2>User Needs</h2>
+		<ul>
+			<li>To connect with like users who are in need to work</li>
+			<li>To connect with like users who have work available</li>
+			<li>To share jobs and installs with picture documentation.</li>
+			<li>To establish a strong profile to appeal to future and or returning customers/fellow employees of the trades
+				(ratings, reviews, and job postings)</li>
+		</ul>
+		<!-- User Frustrations -->
+		<h2>Frustrations</h2>
+		<ul>
+			<li>Slow uploads</li>
+			<li>Lack of coverage out in the field at times</li>
+			<li>Not extremely familiar with computers/apps/and technology in general. Simple is best</li>
+		</ul>
+		<!-- Interaction Flow -->
+		<h2>Interaction  Flow:</h2>
+		<ol>
+			<li>After the user has signed into their already established profile. . </li>
+			<li>User then directed to home page</li>
+			<li>User icons listed next to profile avatar will direct user to post new jobs available</li>
+			<li>User can then list details about the jobs available, if any, or supply further picture documentation for jobs worked</li>
+			<li>User can then provide great detail regarding past/current jobs worked, and available as well as any "helpers" he
+				has worked with in the process.</li>
+			<li>Now that user has provided all necessary information regarding jobs worked and jobs available, they will then be
+			directed to an "Are you satisified with your enteries" page allowing the user to proof read all previous information
+			entered.</li>
+			<li>Once user clicks submit, profile will be available to fellow tradesman to contact for and with work as well as potential
+			employers on larger scales of work. Typically Residential or Commercial employers through large corporations.</li>
+			<li>After the user has completed work for and with en employee, they will be asked to rate their performance, and
+				vice versa. both employer and employee will be rated and will add to an customer based ranking
+				or review to help build the users resume </li>
+		</ol>
+
+		<p><strong>Ultimate Goal:</strong>To be able to provide a profile that is easy to navigate but appealing
+			to potential employers, while also supplying and gaining any work available
+			</p>
+		<p><strong>Audience:</strong> 18 - 55 year old males, tradesman, sub/independent contractors, employers </p>
+		<p><strong>Purpose:</strong> Create a platform for sub and independent contractors may use to gain employment
+			during slow times, and simultaneously build professional profiles and resumes.</p>
